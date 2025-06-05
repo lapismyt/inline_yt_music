70a88dc6a52a05df307d784ecf437817510644f6
Nikita Ugnich <nikitagavrilin005@gmail.com>
2025-06-06 00:44:32 +0300

    removed trash from logging

--- tl_handlers.py
+++ tl_handlers.py
@@ -177,7 +177,7 @@ async def tl_click_download_handler(event: tl_events.CallbackQuery.Event):
                 )
             ]),
         )
-        logger.info(func.stringify())
+        # logger.info(func.stringify())
         await tl_bot(func)
         queued.remove(event.sender_id)
         logger.info("add use")
@@ -232,7 +232,7 @@ async def tl_click_download_handler(event: tl_events.CallbackQuery.Event):
         ]),
     )
     logger.info(func.stringify())
-    await tl_bot(func)
+    # await tl_bot(func)
     await add_use(result_id, event.sender_id)
     logger.info("File downloaded")
 
